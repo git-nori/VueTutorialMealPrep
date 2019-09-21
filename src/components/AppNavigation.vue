dc515027a138a186e590f1865a5f82a29d466f91
HironoriSato <maza.hs5360@gmail.com>
2019-09-21 17:19:58 +0900

    [mod]画面サイズに応じてコンテンツの表示、非表示をする設定を追加

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,12 +1,13 @@
 <template>
     <!-- appに応じて動的にサイズを変更するようappを追加 -->
-    <v-app-bar app color="cyan darken-4" dark>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
-        <v-btn flat color="cyan darken-3">Menu</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn flat color="cyan darken-3">Sign in</v-btn>
-        <v-btn color="cyan lighten-3">JOIN</v-btn>
+    <v-app-bar app color="brown darken-4" dark>
+        <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-spacer class="hidden-md-and-up"></v-spacer>
+        <v-toolbar-title class="hidden-sm-and-down">{{ appTitle }}</v-toolbar-title>
+        <v-btn flat color="brown darken-3">Menu</v-btn>
+        <v-spacer class="hidden-sm-and-down"></v-spacer>
+        <v-btn flat color="brown darken-3" class="hidden-sm-and-down">Sign in</v-btn>
+        <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
     </v-app-bar>
 </template>
 
