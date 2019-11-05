c7c76f21f5a260a765e6f448814b47db7f2376fd
HironoriSato <maza.hs5360@gmail.com>
2019-11-05 16:41:05 +0900

    [add]firebaseをnpmからインストール, index.jsでfirebaseの設定を記述

---
+++ src/firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+// Your web app's Firebase configuration
+const config = {
+  apiKey: "AIzaSyDkgd6QVJhbu52_0H2jUdxmLsPx_STZBS0",
+  authDomain: "meal-prep-b47b8.firebaseapp.com",
+  databaseURL: "https://meal-prep-b47b8.firebaseio.com",
+  projectId: "meal-prep-b47b8",
+  storageBucket: "meal-prep-b47b8.appspot.com",
+  messagingSenderId: "737801899133",
+  appId: "1:737801899133:web:9e6d589e0b852d868cb514",
+  measurementId: "G-15X5YVZWKP"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+
