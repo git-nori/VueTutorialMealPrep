8165be6930ee612fde9734aaf4b36fcf517d66ef
HironoriSato <maza.hs5360@gmail.com>
2019-11-05 12:48:58 +0900

    [add]menu,signin,joinへの遷移をappnavigation.vueに追加

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -20,11 +20,13 @@
         <v-app-bar app color="brown darken-4" dark>
             <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
             <v-spacer class="hidden-md-and-up"></v-spacer>
-            <v-toolbar-title class="hidden-sm-and-down">{{ appTitle }}</v-toolbar-title>
-            <v-btn text color="brown darken-3">Menu</v-btn>
+            <router-link to="/">
+                <v-toolbar-title to="/">{{ appTitle }}</v-toolbar-title>
+            </router-link>
+            <v-btn text color="brown darken-3" to="/menu">Menu</v-btn>
             <v-spacer class="hidden-sm-and-down"></v-spacer>
-            <v-btn text color="brown darken-3" class="hidden-sm-and-down">Sign in</v-btn>
-            <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
+            <v-btn text color="brown darken-3" class="hidden-sm-and-down" to="/signin">Sign in</v-btn>
+            <v-btn color="brown lighten-3" class="hidden-sm-and-down" to="/join">JOIN</v-btn>
         </v-app-bar>
     </span>
 </template>
@@ -43,4 +45,16 @@ export default {
 </script>
 
 <style scoped>
+a {
+    color: white;
+    text-decoration: none;
+}
+.router-link-exact-active {
+    color: white;
+    text-decoration: none;
+}
+.v-toolbar__title {
+    color: white;
+    text-decoration: none;
+}
 </style>
