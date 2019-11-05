1a78d0ffe27a5797fe80f50fa5c0e2903f3bc776
HironoriSato <maza.hs5360@gmail.com>
2019-11-05 12:29:59 +0900

    [mod]vuetifyのバージョン違いによるコンポーネントの記述を修正

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -8,12 +8,12 @@
             disable-resize-watcher
         >
             <v-list>
-                <template v-for="(item, index) in items">
-                    <v-list-title :key="index">
-                        <v-list-title-content>{{ item.title }}</v-list-title-content>
+                <v-list-item v-for="(item, index) in items" :key="index">
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
                         <v-divider :key="`divider-${index}`"></v-divider>
-                    </v-list-title>
-                </template>
+                    </v-list-item-content>
+                </v-list-item>
             </v-list>
         </v-navigation-drawer>
         <!-- appに応じて動的にサイズを変更するようappを追加 -->
@@ -21,9 +21,9 @@
             <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
             <v-spacer class="hidden-md-and-up"></v-spacer>
             <v-toolbar-title class="hidden-sm-and-down">{{ appTitle }}</v-toolbar-title>
-            <v-btn flat color="brown darken-3">Menu</v-btn>
+            <v-btn text color="brown darken-3">Menu</v-btn>
             <v-spacer class="hidden-sm-and-down"></v-spacer>
-            <v-btn flat color="brown darken-3" class="hidden-sm-and-down">Sign in</v-btn>
+            <v-btn text color="brown darken-3" class="hidden-sm-and-down">Sign in</v-btn>
             <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
         </v-app-bar>
     </span>
