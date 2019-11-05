6769b1bba80f1b181955aa809ed6162e5c0e4874
HironoriSato <maza.hs5360@gmail.com>
2019-11-05 11:56:13 +0900

    [add]HomeDetails.vueを新規作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
-    <home-hero></home-hero>
+    <span>
+        <home-hero></home-hero>
+        <home-details></home-details>
+    </span>
 </template>
 
 <script>
 import HomeHero from '@/components/HomeHero';
+import HomeDetails from '@/components/HomeDetails';
 
 export default {
     components: {
-        HomeHero
+        HomeHero,
+        HomeDetails
     }
 };
 </script>
